41411c15c25380dc1ceda224689597bad519d64d
Fuxing Loh <git@fuxing.dev>
2023-06-27 11:09:30 +0800

    remove nativewind

--- app/app.d.ts
+++
@@ -1,4 +0,0 @@
-/**
- * This file is used to declare global types for your application.
- */
-/// <reference types="nativewind/types" />
--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,5 +1,4 @@
 import { Stack } from 'expo-router';
-// import './global.css'
 
 export default function Layout(): JSX.Element {
   return <Stack />;
