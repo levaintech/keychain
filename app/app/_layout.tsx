ce5537502d1b6bb208fb88851bd148a615891682
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2023-06-27 21:27:27 +0800

    chore(app): web preview with netlify.tml (#49)
    <!--  Thanks for sending a pull request! -->
    
    #### What this PR does / why we need it:
    
    Add `netlify.toml` for web preview with a web friendly container of
    `w-[390px] h-[800px]`.

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from 'expo-router';
-import { TailwindProvider } from 'tailwind-rn';
+import { Platform, View } from 'react-native';
+import { TailwindProvider, useTailwind } from 'tailwind-rn';
 
 import utilities from '../tailwind.json';
 
@@ -7,7 +8,21 @@ export default function Layout(): JSX.Element {
   return (
     // @ts-ignore because TailwindProvider is not typed correctly
     <TailwindProvider utilities={utilities}>
-      <Stack />
+      <Container />
     </TailwindProvider>
   );
+}
+
+export function Container(): JSX.Element {
+  const tailwind = useTailwind();
+  if (Platform.OS === 'web') {
+    return (
+      <View style={tailwind('flex-1 justify-center items-center bg-black')}>
+        <View style={tailwind('w-[390px] h-[800px]')}>
+          <Stack />
+        </View>
+      </View>
+    );
+  }
+  return <Stack />;
 }
