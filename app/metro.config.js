a064fff63149b8e634c3176d805d466d98f81fbc
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2023-06-27 19:40:24 +0800

    chore(app): incorporated tailwind-rn watch into metro.config.js (#48)
    <!--  Thanks for sending a pull request! -->
    
    #### What this PR does / why we need it:
    
    For better DX, configured tailwind-rn watch into `metro.config.js`.

---
+++ app/metro.config.js
@@ -0,0 +1,49 @@
+/* eslint-disable */
+const path = require('node:path');
+const fs = require('node:fs/promises');
+const watcher = require('@parcel/watcher');
+const { getDefaultConfig } = require('expo/metro-config');
+const { default: tailwindrn } = require('tailwind-rn/dist/build');
+const tailwind = require('tailwindcss');
+const postcss = require('postcss');
+
+/**
+ * METRO_CONFIG_JS_WATCH=true will enable watch mode that rebuild tailwind.json on file changes.
+ * export METRO_CONFIG_JS_WATCH=true && expo start
+ */
+module.exports = (async () => {
+  const config = getDefaultConfig(__dirname);
+  await postCSS(process.env.METRO_CONFIG_JS_WATCH === 'true');
+  return config;
+})();
+
+/**
+ * Process and build `tailwind.json` the file.
+ *
+ * @param watch {boolean} watch mode
+ */
+async function postCSS(watch) {
+  const processor = postcss([
+    tailwind({
+      corePlugins: require('tailwind-rn/unsupported-core-plugins'),
+      content: ['./app/**/*.{js,jsx,ts,tsx}'],
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+    }),
+  ]);
+
+  async function process() {
+    const result = processor.process(`@tailwind utilities;`);
+    const utilities = tailwindrn(result.css);
+    const content = JSON.stringify(utilities, null, '\t');
+    await fs.writeFile(path.join(__dirname, 'tailwind.json'), content);
+  }
+
+  await process();
+
+  if (watch) {
+    await watcher.subscribe(path.join(__dirname, 'app'), process);
+  }
+}
