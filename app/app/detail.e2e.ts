9e3b52ee8f68b1a90a6d025cecde134437b5d790
Fuxing Loh <git@fuxing.dev>
2023-06-28 16:25:17 +0800

    add fail test example

---
+++ app/app/detail.e2e.ts
@@ -0,0 +1,10 @@
+import { expect, test } from '@playwright/test';
+
+test.describe('Detail', async () => {
+  test('should go to /', async ({ page, baseURL }) => {
+    await page.goto('/');
+
+    await page.getByTestId('GoToDetail').click();
+    await expect(page.getByTestId('DetailScreen')).toHaveText('Fail');
+  });
+});
