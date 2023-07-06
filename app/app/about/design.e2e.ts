83379d70c64878d8f34df497da487b12a82998b8
Fuxing Loh <git@fuxing.dev>
2023-07-06 15:49:35 +0800

    fix tests

--- app/app/about/design.e2e.ts
+++ app/app/about/design.e2e.ts
@@ -3,6 +3,6 @@ import { expect, test } from '@playwright/test';
 test('should go to Settings -> Design System ', async ({ page, baseURL }) => {
   await page.goto('/');
   await page.getByTestId('/tabs/settings').click();
-  await page.getByTestId('/tabs/settings/about/design').click();
-  await expect(page).toHaveURL(`${baseURL}/tabs/settings/about/design`);
+  await page.getByTestId('/about/design').click();
+  await expect(page).toHaveURL(`${baseURL}/about/design`);
 });
--- app/app/index.e2e.ts
+++ app/app/index.e2e.ts
@@ -4,6 +4,5 @@ test.describe('Index', async () => {
   test('should go to /', async ({ page, baseURL }) => {
     await page.goto('/');
     await expect(page).toHaveURL(`${baseURL}/tabs`);
-    await expect(page.getByTestId('KeysPage.Keys')).toContainText('Keys');
   });
 });
